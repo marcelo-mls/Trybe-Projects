f03f7df75abf763f83aaaf9ae9e345c08141c15c
Marcelo Marques <marcelo-mls@hotmail.com>
2022-09-02 19:06:43 -0300

    Ajustando

--- sd-022-b-project-trivia-react-redux/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import playerReducer from './reducer';
-
-const rootReducer = combineReducers({
-  player: playerReducer,
-});
-
-export default rootReducer;
